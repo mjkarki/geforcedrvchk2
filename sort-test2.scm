430abb9179e44d176db8a9f9dcbfe140cf09118e
Matti J. Kärki <mjk@iki.fi>
2019-03-22 17:49:44 +0200

    Added unit tests for sorting algorithm in misctools.scm

---
+++ sort-test2.scm
@@ -0,0 +1,46 @@
+(include "misctools.scm")
+(include "unittest2.scm")
+
+(test "Sort (asc) integer list"
+      (sort <
+            '(7 453 4 5 2 3 90 1 23 15 8 6 0))
+      '(0 1 2 3 4 5 6 7 8 15 23 90 453))
+
+(test "Sort (desc) integer list"
+      (sort >
+            '(7 453 4 5 2 3 90 1 23 15 8 6 0))
+      '(453 90 23 15 8 7 6 5 4 3 2 1 0))
+
+(test "Sort (asc) string list"
+      (sort string<?
+            '("dsfj" "vxc" "gas" "acc" "lass" "Gfd" "ACc" "Libdf"))
+      '("ACc" "Gfd" "Libdf" "acc" "dsfj" "gas" "lass" "vxc"))
+
+(test "Sort (desc) string list"
+      (sort string>?
+            '("dsfj" "vxc" "gas" "acc" "lass" "Gfd" "ACc" "Libdf"))
+      '("vxc" "lass" "gas" "dsfj" "acc" "Libdf" "Gfd" "ACc"))
+
+(define (l<? lst1 lst2)
+  (cond
+   ((< (length lst1) (length lst2))
+    #t)
+   ((> (length lst1) (length lst2))
+    #f)
+   ((and (null? lst1) (null? lst2))
+    #f)
+   ((and (null? lst1) (not (null? lst2)))
+    #t)
+   ((and (not (null? lst1)) (null? lst2))
+    #f)
+   ((= (car lst1) (car lst2))
+    (l<? (cdr lst1) (cdr lst2)))
+   ((< (car lst1) (car lst2))
+    #t)
+   ((> (car lst1) (car lst2))
+    #f)))
+
+(test "Sort (asc) list of lists"
+      (sort l<?
+            '((1 2) (1 0) (1) () (3 2) (3 1 1)))
+      '(() (1) (1 0) (1 2) (3 2) (3 1 1)))
