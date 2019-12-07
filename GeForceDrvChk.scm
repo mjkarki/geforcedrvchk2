0e00f0246543a76fa0657303c77702be8a977a90
Matti J. Kärki <mjk@iki.fi>
2019-12-07 20:36:06 +0200

    Added version information

--- GeForceDrvChk.scm
+++ GeForceDrvChk.scm
@@ -33,6 +33,7 @@
 (include "misctools.scm")
 (include "match.scm")
 
+(define VERSION "1.2")
 (define HOST "gfwsl.geforce.com")
 (define PATH "/services_toolkit/services/com/nvidia/services/AjaxDriverService.php?func=DriverManualLookup&psid=101&pfid=859&osID=57&languageCode=1033&beta=0&isWHQL=1&dltype=-1&sort1=0&numberOfResults=10")
 (define PROGRAMFILES (getenv "ProgramFiles"))
@@ -42,9 +43,10 @@
 (define CMD (string-append WINDIR "\\System32\\" "cmd.exe"))
 
 
-;; Name:       show-error-message
-;; Parameters: nvidiasmi - full path to nvidia-smi.exe
-;; Returns:    #f
+;; Name:        show-error-message
+;; Description: Show error dialog if installed driver version cannot be obtained.
+;; Parameters:  nvidiasmi - full path to nvidia-smi.exe
+;; Returns:     #f
 (define (show-error-message nvidiasmi)
   (message-box (string-append "Unable to get the driver version!"
                               "\n\n"
@@ -58,17 +60,19 @@
   #f)
 
 
-;; Name:       get-program-output 
-;; Parameters: path - full path to an executable
-;; Returns:    executable stdout output
+;; Name:        get-program-output 
+;; Description: Executes another process and grabs anything from the stdout.
+;; Parameters:  path - full path to an executable
+;; Returns:     executable output as a string
 (define (get-program-output path)
   (with-exception-handler (lambda (e) "")
     (lambda () (call-with-input-process (list path: path show-console: #f) port->string))))
 
 
-;; Name:       get-installed-version
-;; Parameters: -
-;; Returns:    version number as a number or #f
+;; Name:        get-installed-version
+;; Description: Gets installed driver version.
+;; Parameters:  -
+;; Returns:     version number as a number or #f
 (define (get-installed-version)
   (let ((matchresult (match "Driver Version: (N.N)"
                        (get-program-output NVIDIASMI))))
@@ -79,11 +83,12 @@
        (show-error-message NVIDIASMI)))))
 
 
-;; Name:       get-driver-info
-;; Parameters: -
-;; Returns:    '(version url) or '(#f #f)
-;;             where version is available driver version as a number
-;;             and url is a download URL of the driver
+;; Name:        get-driver-info
+;; Description: Checks Nvidia web site and fetches the latest available driver version.
+;; Parameters:  -
+;; Returns:     '(version url) or '(#f #f)
+;;              where version is available driver version as a number
+;;              and url is a download URL of the driver
 (define (get-driver-info)
   (let ((page (winhttp-get HOST PATH #t)))
     (cond
@@ -94,10 +99,11 @@
        (list #f #f)))))
 
 
-;; Name:       ask-for-update 
-;; Parameters: installed-version - version number from (get-installed-version)
-;;             version           - version number from (get-driver-info)
-;; Returns:    IDYES or IDNO
+;; Name:        ask-for-update 
+;; Description: Asks if user wants to download the latest driver version.
+;; Parameters:  installed-version - version number from (get-installed-version)
+;;              version           - version number from (get-driver-info)
+;; Returns:     IDYES or IDNO
 (define (ask-for-update installed-version version)
   (message-box (string-append "Current version: "
                               installed-version
@@ -108,19 +114,22 @@
                (bitwise-ior MB_YESNO MB_ICONEXCLAMATION)))
 
 
-;; Name:       open-browser
-;; Parameters: url - opens external browser with URL
-;; Returns:    -
+;; Name:        open-browser
+;; Description: Opens the default system browser using a given URL.
+;; Parameters:  url - address to open
+;; Returns:     -
 (define (open-browser url)
   (open-process (list path: CMD
                       arguments: (list "/c" "start" url)
                       show-console: #f)))
 
 
-;; Name:       main
-;; Parameters: -
-;; Returns:    -
-(define (main)
+;; Name:        check-version
+;; Description: Checks installed driver and available driver versions.
+;;              If newer version is available, asks the user if they want to download it.
+;; Parameters:  -
+;; Returns:     -
+(define (check-version)
   (let* ((info (get-driver-info))
          (version (car info))
          (dlurl (cadr info))
@@ -132,6 +141,42 @@
             (= IDYES (ask-for-update (number->string installed-version)
                                      (number->string version))))
        (open-browser dlurl)))))
+
+
+;; Name:        show-version
+;; Description: Shows version information (this tool and the installed Nvidia driver).
+;; Parameters:  -
+;; Returns:     -
+(define (show-version)
+  (message-box (string-append "Tool version: " VERSION "\n"
+                              "Installed driver version: "
+                              (number->string (get-installed-version)))
+               "Version information"
+               (bitwise-ior MB_OK MB_ICONINFORMATION)))
+
+
+;; Name:        show-help
+;; Description: Shows help information if anything except -v is given at the command line.
+;; Parameters:  -
+;; Returns:     -
+(define (show-help)
+  (message-box "GeForceDrvChk.exe [-v]\n-v shows version information" "Usage" (bitwise-ior MB_OK MB_ICONQUESTION)))
+
+
+;; Name:        main
+;; Description: Equivalent for C main() function. Starts the show.
+;; Parameters:  -
+;; Returns:     -
+(define (main)
+  (cond
+    ((= (length (command-line)) 1)
+     (check-version))
+    ((and (> (length (command-line)) 1)
+          (equal? (cadr (command-line)) "-v"))
+     (show-version))
+    (else
+     (show-help))))
+
 
 (main)
 
