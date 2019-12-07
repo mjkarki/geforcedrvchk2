cb0f440db4af9cc436b8bc76380283734fde14fa
Matti J. Kärki <mjk@iki.fi>
2019-12-07 19:36:31 +0200

    Added debug build option

--- make.bat
+++ make.bat
@@ -1,3 +1,11 @@
 @echo off
 
+if %1x==debugx goto DEBUG
+echo Building release version...
 gsc -exe -cc-options "-mwindows -s -static" -ld-options "-lwinhttp" GeForceDrvChk.scm
+goto END
+:DEBUG
+echo Building debug version...
+gsc -exe -debug -ld-options "-lwinhttp" GeForceDrvChk.scm
+:END
+
